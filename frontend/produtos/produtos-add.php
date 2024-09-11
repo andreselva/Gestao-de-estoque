394a59db5725597f8d778f7033a9a627afee0a7d
Andre Selva <selvaandre99@gmail.com>
2024-09-11 20:41:18 -0300

    ajusta formulario produto

--- frontend/produtos/produtos-add.php
+++ frontend/produtos/produtos-add.php
@@ -22,39 +22,39 @@ require_once __DIR__ . '../../auth.php';
             <div id="input-group-1">
                 <div class="form-group large-input">
                     <label>Nome do produto</label>
-                    <input type="text">
+                    <input type="text" name="name">
                 </div>
                 <div class="form-group medium-input">
                     <label>Código</label>
-                    <input type="text">
+                    <input type="text" name="codigo">
                 </div>
                 <div class="form-group medium-input">
                     <label>Data e hora da criação</label>
-                    <input type="text">
+                    <input type="text" name="dataCriacao">
                 </div>
             </div>
             <div id="input-group-1">
                 <div class="form-group medium-input">
                     <label>Preço de venda</label>
-                    <input type="text">
+                    <input type="text" name="preco-venda">
                 </div>
                 <div class="form-group medium-input">
                     <label>Unidade</label>
-                    <input type="text">
+                    <input type="text" name="un">
                 </div>
                 <div class="form-group medium-input">
                     <label>Peso bruto</label>
-                    <input type="text">
+                    <input type="text" name="peso-bruto">
                 </div>
                 <div class="form-group medium-input">
                     <label>Peso líquido</label>
-                    <input type="text">
+                    <input type="text" name="peso-liquido">
                 </div>
             </div>
             <div id="input-group-1">
-                <div class="form-group medium   -input">
+                <div class="form-group medium-input">
                     <label>GTIN</label>
-                    <input type="text">
+                    <input type="text" name="gtin">
                 </div>
             </div>
             <div class="buttons">
