c4833d742f79552817de342f885b8d8ee0e0d5e0
André Saraiva Selva <121300349+andreselva@users.noreply.github.com>
2024-09-13 11:09:51 -0300

    ajusta método persist

--- src/Produtos/Repository/ProductRepository.php
+++ src/Produtos/Repository/ProductRepository.php
@@ -17,7 +17,20 @@ class ProductRepository implements ProductRepositoryInterface
     public function persist(Product $product): void
     {
         try {
+            $sql = "INSERT INTO products (name, codigo, dataCriacao, precoVenda, un, pesoBruto, pesoLiquido, gtin) 
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+            $stmt = $this->connection->prepare($sql);
+            $stmt->bindValue(1, $product->getName());
+            $stmt->bindValue(2, $product->getCode());
+            $stmt->bindValue(3, $product->getDataCriacao());
+            $stmt->bindValue(4, $product->getPrecoVenda());
+            $stmt->bindValue(5, $product->getUn());
+            $stmt->bindValue(6, $product->getPesoBruto());
+            $stmt->bindValue(7, $product->getPesoLiquido());
+            $stmt->bindValue(8, $product->getGtin());
+            $stmt->execute();
         } catch (Exception $e) {
+            throw new Exception("An error ocurred:" . $e->getMessage());
         }
     }
 }
