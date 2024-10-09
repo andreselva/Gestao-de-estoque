9e51b8088b7e7bd8d6095fbb6920d7ddb3c9c068
André Saraiva Selva <121300349+andreselva@users.noreply.github.com>
2024-10-09 18:05:34 -0300

    salva alteracoes tela de adição de nota

--- frontend/suprimentos/form-nota-entrada-incluir.php
+++ frontend/suprimentos/form-nota-entrada-incluir.php
@@ -12,6 +12,8 @@ require_once __DIR__ . '../../auth.php';
     <title>Produtos</title>
     <link rel="stylesheet" href="../suprimentos/css/nota-entrada.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </head>
 
@@ -20,6 +22,7 @@ require_once __DIR__ . '../../auth.php';
         <main>
             <section id="section-nota">
                 <form id="form-nota-entrada" class="nota-entrada">
+                    <h4>Nota de entrada</h4>
                     <div class="row group-1">
 
                         <div class="col-2 col-date">
@@ -38,40 +41,128 @@ require_once __DIR__ . '../../auth.php';
 
                         <div class="col-1">
                             <label for="numero-nota" class="form-label">Número</label>
-                            <input type="text" class="form-control" name="numero-nota">
+                            <input type="text" class="form-control" id="numero-nota" name="numero-nota">
                         </div>
 
                         <div class="col-2">
                             <label for="nome-empresa" class="form-label">Empresa</label>
-                            <input type="text" class="form-control" name="nome-empresa">
+                            <input type="text" class="form-control" id="nome-empresa" name="nome-empresa">
                         </div>
                         <div class="row">
                             <div class="col-4">
                                 <label for="nat-oper" class="form-label">Natureza de operação</label>
-                                <input type="text" class="form-control" name="nat-oper">
+                                <input type="text" class="form-control" id="nat-oper" name="nat-oper">
                             </div>
                             <div class="col-1">
                                 <label for="numero-nota" class="form-label">Série</label>
-                                <input type="text" class="form-control" name="numero-nota">
+                                <input type="text" class="form-control" id="numero-nota" name="numero-nota">
                             </div>
 
                         </div>
                     </div>
 
+                    <hr class="dropdown-divider">
+
                     <div class="dados-destinatario">
                         <div>
-                            <span>Dados do destinatário</span>
+                            <h5>Dados do destinatário</h5>
                         </div>
 
-                        <div class="col-4">
-                            <label for="data-entrada" class="form-label">Nome</label>
-                            <input type="text" class="form-control" name="data-entrada">
+                        <div class="row group-2">
+
+                            <div class="row">
+                                <div class="col-4">
+                                    <label for="nome-dest" class="form-label">Nome</label>
+                                    <input type="text" class="form-control" id="nome-dest" name="nome-dest">
+                                </div>
+                                <div class="col-2 col-type">
+                                    <label for="pessoaTipo" class="form-label">Pessoa</label>
+                                    <select class="form-select select-input" id="pessoaTipo">
+                                        <option selected value="p-fisica">Física</option>
+                                        <option value="p-juridica">Jurídica</option>
+                                    </select>
+                                </div>
+                            </div>
+
+
+                            <div class="row">
+                                <div class="col-2" id="cpfCnpjContainer">
+                                    <label for="documentField" class="form-label" id="documentLabel">CPF</label>
+                                    <input type="text" class="form-control" id="documentField" name="cpf-cnpj" placeholder="CPF ou CNPJ" maxlength="14">
+                                </div>
+                                <div class="col-2" id="ieField" style="display: none">
+                                    <label for="ieField" class="form-label">Inscrição Estadual</label>
+                                    <input type="text" class="form-control" id="ieField" placeholder="Digite a IE">
+                                </div>
+                                <div class="col-2" id="contribuinteField" style="display: none">
+                                    <label for="identificadorField" class="form-label">Identificador</label>
+                                    <select class="form-select" id="identificadorField">
+                                        <option selected disabled value="">Selecione...</option>
+                                        <option value="contribuinte">Contribuinte</option>
+                                        <option value="nao-contribuinte">Não Contribuinte</option>
+                                    </select>
+                                </div>
+                            </div>
+
+
+                            <div class="row">
+                                <div class="col-2 type-cep">
+                                    <label for="cep" class="form-label">CEP</label>
+                                    <div class="input-group mb-3">
+                                        <input type="text" class="form-control inpt-cep" id="cep" name="cep">
+                                        <button class="btn btn-outline-secondary btn-cep" type="button" id="button-addon1">
+                                            <ion-icon class="icon" name="location-outline"></ion-icon>
+                                        </button>
+                                    </div>
+                                </div>
+                                <div class="col-4">
+                                    <label for="endereco" class="form-label">Endereço</label>
+                                    <input type="text" class="form-control" id="endereco" name="endereco">
+                                </div>
+                                <div class="col-1">
+                                    <label for="numero-endereco" class="form-label">Número</label>
+                                    <input type="text" class="form-control" id="numero-endereco" name="numero-endereco">
+                                </div>
+                                <div class="col-2">
+                                    <label for="complemento" class="form-label">Complemento</label>
+                                    <input type="text" class="form-control" id="complemento" name="complemento">
+                                </div>
+                                <div class="row">
+                                    <div class="col-2">
+                                        <label for="bairro" class="form-label">Bairro</label>
+                                        <input type="text" class="form-control" id="bairro" name="bairro">
+                                    </div>
+                                    <div class="col-3">
+                                        <label for="cidade" class="form-label">Cidade</label>
+                                        <input type="text" class="form-control" id="cidade" name="cidade">
+                                    </div>
+                                    <div class="col-1 uf">
+                                        <label for="uf" class="form-label">UF</label>
+                                        <select class="form-select" id="uf">
+                                            <option selected disabled value="">Selecione...</option>
+                                            <option value="RS">RS</option>
+                                            <option value="SC">SC</option>
+                                        </select>
+                                    </div>
+                                    <div class="col-2">
+                                        <label for="phone" class="form-label">Telefone</label>
+                                        <input type="text" class="form-control" id="phone" name="phone">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-3">
+                                    <label for="email" class="form-label">E-mail</label>
+                                    <input type="email" class="form-control" id="email" name="email">
+                                </div>
+                            </div>                       
+
                         </div>
 
                     </div>
 
-
                 </form>
+
             </section>
         </main>
     </div>
--- frontend/suprimentos/js/form-nota-entrada-controller.js
+++ frontend/suprimentos/js/form-nota-entrada-controller.js
@@ -2,3 +2,42 @@ function goToAddEntryNote(event) {
     event.preventDefault();
     window.location.href = "../suprimentos/form-nota-entrada-incluir.php"
 }
+
+document.addEventListener('DOMContentLoaded', function () {
+    // Seleciona o elemento do tipo de pessoa (física ou jurídica)
+    const pessoaTipo = document.getElementById('pessoaTipo');
+
+    // Seleciona o campo de documento e o label
+    const documentField = document.getElementById('documentField');
+    const documentLabel = document.getElementById('documentLabel');
+    const ieField = document.getElementById('ieField');
+    const contribuinteField = document.getElementById('contribuinteField');
+
+    // Evento de mudança no select de tipo de pessoa
+    pessoaTipo.addEventListener('change', function () {
+        const selectedValue = this.value;
+
+        if (selectedValue === 'p-fisica') {
+            // Alterar para CPF
+            documentLabel.textContent = 'CPF';
+            documentField.setAttribute('placeholder', 'Digite o CPF');
+            documentField.setAttribute('maxlength', '14');
+            documentField.setAttribute('pattern', '\\d{14}');
+
+            ieField.style.display = 'none';
+            contribuinteField.style.display = 'none';
+
+        } else if (selectedValue === 'p-juridica') {
+            // Alterar para CNPJ
+            documentLabel.textContent = 'CNPJ';
+            documentField.setAttribute('placeholder', 'Digite o CNPJ');
+            documentField.setAttribute('maxlength', '18');
+            documentField.setAttribute('pattern', '\\d{14}');
+
+            ieField.style.removeProperty('display');
+            contribuinteField.style.removeProperty('display');
+        }
+    });
+});
+
+
